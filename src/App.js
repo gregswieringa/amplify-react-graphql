6cdebdc989a4d50470e6ed28559863d22fc571c8
Greg Swieringa <gregswieringa@gmail.com>
2024-04-14 16:23:07 -0400

    fetchback

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('http://3.92.223.134/')
+      .then(response => response.text())
+      .then(data => setData(data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from Version V4</h1>
+        {data ? <p>{data}</p> : 'Loading...'}
       </header>
     </div>
   );
